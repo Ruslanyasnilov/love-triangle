b245cfa86910ff887a4d3ac47b21f5188f1896fd
Ruslan Yasnilov <ruslanyasnilov@yandex.ru>
2019-07-04 21:07:09 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (i = 0; i < preferences.length; i++) {
+    const Sp1 = preferences[i];
+    const Sp2 = preferences[Sp1 - 1];
+    const Sp3 = preferences[Sp2 - 1];
+    if (Sp3 === i + 1 && Sp1 !== i + 1) {
+      count++;
+    }
+  }
+  return count / 3;
 };
